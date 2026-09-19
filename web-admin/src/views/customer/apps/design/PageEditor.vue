d61a1610883471002420d8146bf2a29683d00927
LinPing <linping@LINPING-MAC.lan>
2026-09-19 13:36:01 +0800

    style: switchColor控件checkbox改为el-switch开关样式

--- web-admin/src/views/customer/apps/design/PageEditor.vue
+++ web-admin/src/views/customer/apps/design/PageEditor.vue
@@ -393,7 +393,7 @@
                   </div>
                   <el-switch v-else-if="f.control === 'switch'" v-model="selectedComp.props[f.key]" />
                   <div v-else-if="f.control === 'switchColor'" style="display:flex;align-items:center;gap:8px;width:100%;">
-                    <el-checkbox :model-value="selectedComp.props[f.key]" @change="v => selectedComp.props[f.key] = v" />
+                    <el-switch :model-value="selectedComp.props[f.key]" @change="v => selectedComp.props[f.key] = v" />
                     <el-color-picker v-model="selectedComp.props[f.colorKey]" size="small" />
                     <span style="font-size:12px;color:#86909C;flex:1;">{{ selectedComp.props[f.colorKey] }}</span>
                   </div>
